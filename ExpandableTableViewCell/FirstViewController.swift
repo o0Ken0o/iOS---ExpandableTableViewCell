f5555a95007ab9d3ea3f8ee1298037b1f2546f0c
Ken Siu <kensiu20042000@gmail.com>
2018-01-07 11:56:34 +0800

    add cocoapod to and remove main storyboard from the project

---
+++ ExpandableTableViewCell/FirstViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FirstViewController.swift
+//  ExpandableTableViewCell
+//
+//  Created by Kam Hei Siu on 7/1/2018.
+//  Copyright © 2018 Kam Hei Siu. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = "Expandable Cells"
+    }
+}
+
